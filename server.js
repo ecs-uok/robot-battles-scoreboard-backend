80c0e42a3e30829d31770cb5350dba812b44ec51
Binath Perera <binathperera@bluelotus360.co>
2024-06-04 20:19:43 +0530

    add gitignore

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express= require('express');
 const cors= require('cors');
+
+
+const port = process.env.PORT || 5000;
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -96,4 +100,4 @@ app.get("/teams", (req,res)=>{
     res.json({"users": ["user1","user2"]});
 })
 
-app.listen(5000, ()=> {console.log("Server started on port 5000")})
+app.listen(port, ()=> {console.log(`Server started on port ${port}`)})
